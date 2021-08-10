ac62d3cac2f0dc5ca183d69a60f4054ba45063ad
Mitali <mitaliagrawal4321@gmail.com>
2021-08-10 14:06:20 +0530

    Added project7

---
+++ 07AnimatedLogin/app/src/main/java/com/example/android/animatedlogin/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.animatedlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+    }
+}
