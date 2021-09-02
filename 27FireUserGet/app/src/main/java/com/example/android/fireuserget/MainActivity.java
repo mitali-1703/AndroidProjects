6356a227332ccfb572f2e075144332c769beb8c6
Mitali <mitaliagrawal4321@gmail.com>
2021-09-03 01:27:05 +0530

    Getting complex data from database via FirebaseDatabase

--- 27FireUserGet/app/src/main/java/com/example/android/fireuserget/MainActivity.java
+++ 27FireUserGet/app/src/main/java/com/example/android/fireuserget/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.android.fireuserget;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    String name;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference rootRef = db.getReference();
+    DatabaseReference myRef = rootRef.child("Users");
+
+    ListView listView;
+    ArrayList<String> usernames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,usernames);
+        listView.setAdapter(adapter);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            //onChildAdded method shows data of existing as well as newly created child and snapshot gives data in json format
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue().toString();
+
+                try {
+                    JSONObject object = new JSONObject(value);
+                    name = object.getString("Email");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                usernames.add(value);
+                usernames.add(name);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
