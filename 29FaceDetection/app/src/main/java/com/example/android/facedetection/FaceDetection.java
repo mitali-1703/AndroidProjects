66954a3ef0ec3b38232ea7c762c90e244925b52e
Mitali <mitaliagrawal4321@gmail.com>
2021-09-18 23:50:05 +0530

    Added layout files, firebase face-detection ml-kit and connected it to firebase

---
+++ 29FaceDetection/app/src/main/java/com/example/android/facedetection/FaceDetection.java
@@ -0,0 +1,8 @@
+package com.example.android.facedetection;
+
+import android.app.Application;
+
+public class FaceDetection extends Application {
+    public static final String RESULT_TEXT = "RESULT_TEXT";
+    public static final String RESULT_DIALOG = "RESULT_DIALOG";
+}
