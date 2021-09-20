c6fdb47c9547a28fd4cdc9aff8ede12386173fc3
Mitali <mitaliagrawal4321@gmail.com>
2021-09-21 02:13:15 +0530

    Added layout files and created reference for firebase storage

---
+++ 28FireFileUploader/app/src/main/java/com/example/android/firefileuploader/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.android.firefileuploader;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    StorageReference mStorage;
+    private static final int GALLERY_REQUEST = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityIfNeeded(intent,GALLERY_REQUEST);
+            }
+        });
+    }
+
+    //When the above method provides images on the screen then what should happen--->
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST){
+            Uri uri = data.getData();
+            
+        }
+    }
+}
