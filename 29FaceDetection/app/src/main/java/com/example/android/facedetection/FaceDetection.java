6da10a3d8b5089fb740db68e29ff8e4012e5c31d
Mitali Agrawal <66427936+mitali-1703@users.noreply.github.com>
2021-09-25 17:49:58 +0530

    Initialized FirebaseApp by overriding onCreate

--- 29FaceDetection/app/src/main/java/com/example/android/facedetection/FaceDetection.java
+++ 29FaceDetection/app/src/main/java/com/example/android/facedetection/FaceDetection.java
@@ -6,5 +6,9 @@ public class FaceDetection extends Application {
     public static final String RESULT_TEXT = "RESULT_TEXT";
     public static final String RESULT_DIALOG = "RESULT_DIALOG";
 
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
 }
