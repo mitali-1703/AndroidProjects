3dc4ae2b52559488c428697acdabd5479af13d4c
Mitali Agrawal <66427936+mitali-1703@users.noreply.github.com>
2021-09-25 17:52:53 +0530

    Added permission to capture image from camera

--- 29FaceDetection/app/src/main/java/com/example/android/facedetection/MainActivity.java
+++ 29FaceDetection/app/src/main/java/com/example/android/facedetection/MainActivity.java
@@ -7,12 +7,32 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button openCamera;
-    private final static int REQUEST_CAMERA = 123;
+    private Button cameraButton;
+    
+    //Whenever using your own custom permissions then we need to use a flg value(which is an integer) to have a check
+    //whether the user has granted our pp that permission or not
+    private final static int REQUEST_IMAGE_CAPTURE = 124;
+    private InputImage image;
+    private FaceDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        FirebaseApp.initializeApp(this);
+        cameraButton = findViewById(R.id.camera_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                
+                //Checking if the user has given correct permission to our app 
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 }
