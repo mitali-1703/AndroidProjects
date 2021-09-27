25cc2cfde61d223fdc5b94d2bbe8184065a1d307
Mitali <mitaliagrawal4321@gmail.com>
2021-09-27 10:08:22 +0530

    Added camera permissions for the app

--- 30TextRecognization/app/src/main/java/com/example/android/textrecognition/MainActivity.java
+++ 30TextRecognization/app/src/main/java/com/example/android/textrecognition/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.android.textrecognition;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewOnReceiveContentListener;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button openCamera;
+    private static final int REQUEST_IMAGE_CAPTURE = 56;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                    startActivityIfNeeded(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+//            Bundle bundle = ;
+
+        }
     }
 }
